89362837c23db11aa10b6596fe64e0d71a42caf5
vigneshraaj <vigneshraaj19@gmail.com>
2022-08-30 11:31:32 +0530

    node

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
