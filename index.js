d0186fa9db922850859724adec04647fa98df4c3
vigneshraaj <vigneshraaj19@gmail.com>
2022-08-30 11:29:51 +0530

    update

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 const PORT=process.env.PORT || 5000;
-require("dotenv").config();
+// require("dotenv").config();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
+
+const cors = require("cors"); 
+dotenv.config(); 
+app.use( cors({ origin: "*", }) );
+ app.use(express.json());
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
