746bd786ec5456c6ef6b8f15abf869e26fe8d934
vigneshraaj <vigneshraaj19@gmail.com>
2022-08-30 17:58:14 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,24 +2,16 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
 module.exports.login = async (req, res, next) => {
-  
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    
-    if (!user) 
-    {
+    if (!user)
       return res.json({ msg: "Incorrect Username or Password", status: false });
-    }
     const isPasswordValid = await bcrypt.compare(password, user.password);
-      
     if (!isPasswordValid)
-    {
       return res.json({ msg: "Incorrect Username or Password", status: false });
     delete user.password;
     return res.json({ status: true, user });
-    }
-      
   } catch (ex) {
     next(ex);
   }
